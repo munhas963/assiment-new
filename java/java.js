8708e9d15d20deab4a7f1bc8343d5b52107c02f0
munhas963 <munnahassan963.m@gmail.com>
2025-03-03 14:27:49 +0600

    last time try

---
+++ java/java.js
@@ -0,0 +1,323 @@
+document.getElementById('add-btn1').addEventListener('click', function(){
+    alert('Borad updated succcessfully')
+  
+  
+    const btn = document.getElementById("btn1").innerText;
+
+const convatBtn =parseFloat(btn) -1;
+document.getElementById("btn1").innerText=convatBtn;
+
+
+const button = document.getElementById("btn2").innerText;
+const convartMain = parseFloat(button) +1;
+document.getElementById("btn2").innerText =convartMain;
+
+
+let opacityBtn =document.getElementById('add-btn1');
+opacityBtn.style.opacity = "0.2"
+opacityBtn.style.backgroundColor ="bg-indigo-600";
+opacityBtn.style.pointerEvents ="none";
+if(convartMain > 29){
+    alert("congratulations!! You have completed all the current task ")
+}
+let time = new Date();
+let formetTime =time.toLocaleString('es-Us',{
+    hour:'2-digit',
+    minute:"2-digit",
+    second:'2-digit',
+    hour12:true
+});
+let now =document.getElementById("fix-phone");
+let nowNam = now.innerText;
+let active =document.getElementById("logos");
+let paragraph =document.createElement('p');
+
+paragraph.classList.add(
+    'p-4',
+    'bg-[#F4F7FF]',
+    'rounded-lg',
+    'shadow-md', 'text-gray-700','mt-3',
+    'mx-2'
+);
+paragraph.innerText =`You have Complete The TaskFix Mobile Button Issue   ${formetTime}`;
+active.appendChild(paragraph)
+
+})
+
+// btn2
+document.getElementById('add-btn2').addEventListener('click', function(){
+    alert('Borad updated succcessfully')
+    const btn = document.getElementById("btn1").innerText;
+
+const convatBtn =parseFloat(btn) -1;
+document.getElementById("btn1").innerText=convatBtn;
+
+const button = document.getElementById("btn2").innerText;
+const convartMain = parseFloat(button) +1;
+document.getElementById("btn2").innerText =convartMain;
+
+
+let opacityBtn =document.getElementById('add-btn2');
+opacityBtn.style.opacity = "0.2"
+opacityBtn.style.backgroundColor ="bg-indigo-600";
+opacityBtn.style.pointerEvents ="none";
+if(convartMain >29){
+    alert("congratulations!! You have completed all the current task ")
+}
+let time = new Date();
+let formetTime =time.toLocaleString('es-Us',{
+    hour:'2-digit',
+    minute:"2-digit",
+    second:'2-digit',
+    hour12:true
+});
+let now =document.getElementById("fix-phone");
+let nowNam = now.innerText;
+let active =document.getElementById("logos");
+let paragraph =document.createElement('p');
+
+paragraph.classList.add(
+    'p-4',
+    'bg-[#F4F7FF]',
+    'rounded-lg',
+    'shadow-md', 'text-gray-700','mt-3',
+    'mx-2'
+);
+paragraph.innerText =`You have Complete The Task Add Dark Mode  ${formetTime}`;
+active.appendChild(paragraph)
+
+
+
+})
+
+// btn3
+
+document.getElementById('add-btn3').addEventListener('click', function(){
+    alert('Borad updated succcessfully')
+    const btn = document.getElementById("btn1").innerText;
+
+const convatBtn =parseFloat(btn) -1;
+document.getElementById("btn1").innerText=convatBtn;
+
+
+const button = document.getElementById("btn2").innerText;
+const convartMain = parseFloat(button) +1;
+document.getElementById("btn2").innerText =convartMain;
+
+
+let opacityBtn =document.getElementById('add-btn3');
+opacityBtn.style.opacity = "0.2"
+opacityBtn.style.backgroundColor ="bg-indigo-600";
+opacityBtn.style.pointerEvents ="none";
+if(convartMain > 29){
+    alert("congratulations!! You have completed all the current task ")
+}
+let time = new Date();
+let formetTime =time.toLocaleString('es-Us',{
+    hour:'2-digit',
+    minute:"2-digit",
+    second:'2-digit',
+    hour12:true
+});
+let now =document.getElementById("fix-phone");
+let nowNam = now.innerText;
+let active =document.getElementById("logos");
+let paragraph =document.createElement('p');
+
+paragraph.classList.add(
+    'p-4',
+    'bg-[#F4F7FF]',
+    'rounded-lg',
+    'shadow-md', 'text-gray-700','mt-3',
+    'mx-2'
+);
+paragraph.innerText =`You have Complete The TaskOptimize  Home page   ${formetTime}`;
+active.appendChild(paragraph)
+
+
+
+
+})
+
+
+// btn4
+document.getElementById('add-btn4').addEventListener('click', function(){
+    alert('Borad updated succcessfully')
+    const btn = document.getElementById("btn1").innerText;
+
+const convatBtn =parseFloat(btn) -1;
+document.getElementById("btn1").innerText=convatBtn;
+
+
+const button = document.getElementById("btn2").innerText;
+const convartMain = parseFloat(button) +1;
+document.getElementById("btn2").innerText =convartMain;
+
+
+
+let opacityBtn =document.getElementById('add-btn4');
+opacityBtn.style.opacity = "0.2"
+opacityBtn.style.backgroundColor ="bg-indigo-600";
+opacityBtn.style.pointerEvents ="none";
+if(convartMain > 29){
+    alert("congratulations!! You have completed all the current task ")
+}
+let time = new Date();
+let formetTime =time.toLocaleString('es-Us',{
+    hour:'2-digit',
+    minute:"2-digit",
+    second:'2-digit',
+    hour12:true
+});
+let now =document.getElementById("fix-phone");
+let nowNam = now.innerText;
+let active =document.getElementById("logos");
+let paragraph =document.createElement('p');
+
+paragraph.classList.add(
+    'p-4',
+    'bg-[#F4F7FF]',
+    'rounded-lg',
+    'shadow-md', 'text-gray-700','mt-3',
+    'mx-2'
+);
+paragraph.innerText =`You have Complete The Task Add new emoji 🤲  ${formetTime}`;
+active.appendChild(paragraph)
+
+
+}) 
+// btn-5
+
+document.getElementById('add-btn5').addEventListener('click', function(){
+    alert('Borad updated succcessfully')
+    const btn = document.getElementById("btn1").innerText;
+
+const convatBtn =parseFloat(btn) -1;
+document.getElementById("btn1").innerText=convatBtn;
+
+
+const button = document.getElementById("btn2").innerText;
+const convartMain = parseFloat(button) +1;
+document.getElementById("btn2").innerText =convartMain;
+
+
+let opacityBtn =document.getElementById('add-btn5');
+opacityBtn.style.opacity = "0.2"
+opacityBtn.style.backgroundColor ="bg-indigo-600";
+opacityBtn.style.pointerEvents ="none";
+if(convartMain > 29){
+    alert("congratulations!! You have completed all the current task ")
+}
+let time = new Date();
+let formetTime =time.toLocaleString('es-Us',{
+    hour:'2-digit',
+    minute:"2-digit",
+    second:'2-digit',
+    hour12:true
+});
+let now =document.getElementById("fix-phone");
+let nowNam = now.innerText;
+let active =document.getElementById("logos");
+let paragraph =document.createElement('p');
+
+paragraph.classList.add(
+    'p-4',
+    'bg-[#F4F7FF]',
+    'rounded-lg',
+    'shadow-md', 'text-gray-700','mt-3',
+    'mx-2'
+);
+paragraph.innerText =`You have Complete The TaskIntegrate OpenAI API  ${formetTime}`;
+active.appendChild(paragraph)
+
+
+})
+
+// btn-6
+
+document.getElementById('add-btn6').addEventListener('click', function(){
+    alert('Borad updated succcessfully')
+
+    
+let opacityBtn =document.getElementById('add-btn6');
+opacityBtn.style.opacity = "0.2"
+opacityBtn.style.backgroundColor ="bg-indigo-600";
+opacityBtn.style.pointerEvents ="none";
+if(convartMain > 29){
+    alert("congratulations!! You have completed all the current task ")
+}
+let time = new Date();
+let formetTime =time.toLocaleString('es-Us',{
+    hour:'2-digit',
+    minute:"2-digit",
+    second:'2-digit',
+    hour12:true
+});
+let now =document.getElementById("fix-phone");
+let nowNam = now.innerText;
+let active =document.getElementById("logos");
+let paragraph =document.createElement('p');
+
+paragraph.classList.add(
+    'p-4',
+    'bg-[#F4F7FF]',
+    'rounded-lg',
+    'shadow-md', 'text-gray-700','mt-3',
+    'mx-2'
+);
+paragraph.innerText =`You have Complete The Task Add Improve Job searching   ${formetTime}`;
+active.appendChild(paragraph)
+
+})
+document.getElementById('fix-phone').addEventListener('click',function(){
+    const parent =document.getElementById('fix-phone');
+    const child =document.getElementById('logos');
+    parent.removeChild(child);
+})
+
+document.getElementById('question').addEventListener('click',function(){
+    window.location.href="./new.html";
+     
+})
+//  document.getElementById('back').addEventListener('click',function(){
+//     window.location.href="./index.html";
+//   })
+
+const image = document.getElementById('image-color');
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  image.addEventListener('click', () => {
+    document.body.style.backgroundColor = getRandomColor();
+  });
+
+
+
+    
+
+  function displayDateAndDay() {
+    const nowDay = new Date();
+    
+    
+    const day = nowDay.toLocaleString('en-US', { weekday: 'long' }); 
+    const date = nowDay.getDate(); 
+    const month = nowDay.toLocaleString('en-US', { month: 'long' }); 
+    const year = nowDay.getFullYear(); 
+
+    
+    const formattedDate = `${day}, ${month} ${date}, ${year}`;
+
+    document.getElementById('dateDay').innerText = formattedDate;
+}
+
+
+displayDateAndDay();
+
+
+    
